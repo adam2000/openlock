892c1adf8efb1601109dc346a73a0e7a9b064d3f
kristoffer.skulp <kristoffer.skulp@gmail.com>
2013-04-30 16:23:22 +0000

    more module load at startup

--- perl/startup.pl
+++ perl/startup.pl
@@ -2,8 +2,8 @@
 
 use lib qw( /etc/apache2/perl );
 
-#use Apache2::AuthTicket;
-#use ModPerl::Registry;
+use Apache2::AuthTicket;
+use ModPerl::Registry;
 use LockServer::Db;
 use Embperl;
 use AnyEvent::JSONRPC::TCP::Client;
